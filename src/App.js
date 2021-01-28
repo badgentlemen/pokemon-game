edae3151876490e2e76f7395b1eaf10c1d8ea0ee
Дудуев Мухамед <duduevmuhammed@gmail.com>
2021-01-28 14:27:00 +0300

    Homework 2
    Finished homework 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Layout from './Components/Layout';
+import Footer from './Components/Footer';
+import FirstLayoutBG from './Assets/bg2.jpg';
+import ThirdLayoutBG from './Assets/bg1.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.JS!
-        </p>
-      </header>
+      <Header title="Pokemon Game" descr="react marathon!"/>
+      <Layout id={1} title="Первый Layout" descr="Описание для первого layout!" urlBg={FirstLayoutBG}/>
+      <Layout id={2} title="Второй Layout" descr="Описание для второго layout!" colorBg="purple"/>
+      <Layout id={3} title="Третий Layout" descr="Описание для третьего layout!" urlBg={ThirdLayoutBG}/>
+      <Footer />
     </div>
   );
 }
