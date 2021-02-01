1fa3c798321196ac2764ac0ee726f88471154785
Дудуев Мухамед <duduevmuhammed@gmail.com>
2021-02-01 18:46:36 +0300

    added home and game pages, and handling page changes

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,13 +1,22 @@
+import { ReactNode } from 'react';
 import style from './style.module.css';
 
+interface HeaderProps {
+    title?: ReactNode;
+    descr?: ReactNode;
+    onButtonClick?: () => void;
+}
 
-const Header = ({title, descr}) => {
+const Header = ({title, descr, onButtonClick}: HeaderProps) => {
     return (
         <header className={style.root}>
             <div className={style.forest}></div>
             <div className={style.container}>
                 { title && <h1>{title}</h1> }
                 { descr && <p>{descr}</p> }
+                <button onClick={onButtonClick}>
+                    Start Game
+                </button>
             </div>
         </header>
     )
---
+++ src/Routes/Game/index.tsx
@@ -0,0 +1,6 @@
+
+export const GamePage = (): JSX.Element => (
+    <div className="game-page">
+
+    </div>
+)
