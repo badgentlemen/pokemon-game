c699e96807158883e34ffc95ed663dbf7b102b5f
Дудуев Мухамед <duduevmuhammed@gmail.com>
2021-02-14 00:02:26 +0300

    fetching enemy pokemons

--- src/Service/Api/index.ts
+++ src/Service/Api/index.ts
@@ -1,5 +1,20 @@
+import { Pokemon } from '../../Interfaces';
 import { Cell } from '../../Interfaces/Cell';
 
 export * from './Firebase';
 
-export const fetchBoardCells = (): Promise<Cell[]> => fetch('https://reactmarathon-api.netlify.app/api/board').then(response => response.json()).then(json => json.data || []);
+interface InvokeApiWithData<T> {
+    data: T
+}
+
+async function invokeApi<T>(url: string, data?: any): Promise<T> {
+    const response = await fetch(url, {
+        body: data
+    });
+    const json = await response.json();
+    return json;
+}
+
+export const fetchBoardCells = async (): Promise<Cell[]> => (await invokeApi<InvokeApiWithData<Cell[]>>('https://reactmarathon-api.netlify.app/api/board')).data;
+
+export const getEnemyPokemons = async (): Promise<Pokemon[]> => (await invokeApi<InvokeApiWithData<Pokemon[]>>('https://reactmarathon-api.netlify.app/api/create-player')).data;
